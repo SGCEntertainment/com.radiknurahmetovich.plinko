72e95611fae1ee347dcf851210c3c37405c095b9
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-12-06 18:58:43 +0300

    update

--- Assets/plinko.scr.BTST-En-27112y/Scripts/Managers/SFXManager.cs
+++ Assets/plinko.scr.BTST-En-27112y/Scripts/Managers/SFXManager.cs
@@ -6,14 +6,14 @@ public class SFXManager : MonoBehaviour
 
     private void Awake()
     {
-        Player.OnCollidedGold += () =>
-        {
-            if(sfxSource.isPlaying)
-            {
-                sfxSource.Stop();
-            }
+        //Player.OnCollidedGold += () =>
+        //{
+        //    if(sfxSource.isPlaying)
+        //    {
+        //        sfxSource.Stop();
+        //    }
 
-            sfxSource.Play();
-        };
+        //    sfxSource.Play();
+        //};
     }
 }
