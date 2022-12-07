1d7b401637850c0991a9f1f3514ed087c1dac12e
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-12-07 04:37:46 +0300

    update

--- Assets/plinko.scr.BTST-En-27112y/Scripts/Managers/UIManager.cs
+++ Assets/plinko.scr.BTST-En-27112y/Scripts/Managers/UIManager.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    
+    private void Start()
+    {
+        GameObject prefab = Resources.Load<GameObject>("game");
+        Instantiate(prefab, GameObject.Find("main").transform);
+    }
 }
